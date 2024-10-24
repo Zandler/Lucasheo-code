22f724550fbebbc0a5652d5192b6c011e8e24a13
Lucas Henrique Evangelista <lucasheo2004@hotmail.com>
2024-10-23 22:12:14 -0300

    add module vpc

--- ops/main.tf
+++ ops/main.tf
@@ -1,38 +1,25 @@
 provider "aws" {
-  region = local.region
+  region = var.aws_region
 
 }
 
-data "aws_availability_zones" "available" {}
+##  VPC
 
-locals {
-  name   = "ex-${basename(path.cwd)}"
-  region = "us-east-1"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  name = "lab-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
 
   tags = {
-    name    = local.name
-    lab     = lucasheo
-    
+    Terraform   = "true"
+    Environment = "lab-dev"
   }
 }
-
-
-# VPC
-
-module "vpc_example_simple" {
-  source  = "terraform-aws-modules/vpc/aws//examples/simple"
-  version = "5.14.0"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-
-  tags = local.tags
-
-}
-
