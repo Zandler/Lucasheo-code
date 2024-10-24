baec02f6078237fab018c34a69d7799f73551858
Lucas Henrique Evangelista <lucasheo2004@hotmail.com>
2024-10-23 22:44:41 -0300

    add module alb

--- ops/main.tf
+++ ops/main.tf
@@ -381,3 +381,70 @@ resource "aws_cloudfront_function" "example" {
   runtime = "cloudfront-js-1.0"
   code    = file("${path.module}/example-function.js")
 }
+
+
+
+####### ALB
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "lab-dev"
+  vpc_id  = "vpc-abcde012"
+  subnets = ["subnet-abcde012", "subnet-bcde012a"]
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = "i-0f6d38a07d50d080f"
+    }
+  }
+
+  tags = {
+    Environment = "lab-dev"
+    Project     = "lab-dev-Example"
+  }
+}
